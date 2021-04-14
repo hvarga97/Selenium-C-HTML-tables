ca4fc45135977eaf9ffe0fc937c4a88c8965d5fc
Hrvoje <hvarga97@gmail.com>
2021-04-14 18:16:13 +0200

    Add files via upload

---
+++ Base.cs
@@ -0,0 +1,17 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingTable
+{
+    public class Base
+    {
+
+        public static IWebDriver Driver;
+
+
+    }
+}
