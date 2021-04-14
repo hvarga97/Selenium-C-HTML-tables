210a2341edd1b4a0314c6d16482da09901f95ec5
Hrvoje <hvarga97@gmail.com>
2021-04-14 18:19:23 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using OpenQA.Selenium.Firefox;
+using OpenQA.Selenium.Firefox;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,36 +11,29 @@ namespace ReadingTable
     {
         static void Main(string[] args)
         {
-
             Driver = new FirefoxDriver();
-
-            //Driver.Navigate().GoToUrl("file://///Mac/Home/Documents/SimpleTable.html");
+            //Driver.Navigate().GoToUrl("file://///Mac/Home/Documents/SimpleTable.html"); - when using SimpleTable.html
+            
         //For complex tables
             Driver.Navigate().GoToUrl("file://///Mac/Home/Documents/ComplexTable.html");
         //..
-
+        
             TablePage page = new TablePage();
-
             //Read the table
             Utilities.ReadTable(page.table);
-
             //Get the cell value from the table
             Console.WriteLine(Utilities.ReadCell("Email", 1));
-
             Console.WriteLine("The name {0} with Email {1} and Phone {2}",
                 Utilities.ReadCell("Name",2), Utilities.ReadCell("Email", 2), Utilities.ReadCell("Phone", 2));
-
+                
          //For complex tables
             //delete Marko row
             //Utilities.PerformActionOnCell("5", "Name", "Marko", "Delete"); //example of deleting a row
             //Utilities.PerformActionOnCell("5", "Name", "Marko", "Save"); //example of saving the row
             Utilities.PerformActionOnCell("Option", "Name", "Marko");
-            //..
-
+         //..
+         
             Console.Read();
-
-
-
         }
     }
 }
