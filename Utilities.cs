5ada53c11fdb336fb5022a651465c5a3e2984fb4
Hrvoje <hvarga97@gmail.com>
2021-04-14 18:20:25 +0200

    Add files via upload

---
+++ Utilities.cs
@@ -0,0 +1,118 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingTable
+{
+    public class Utilities
+    {
+
+        static List<TableDataCollection> _tableDataCollection = new List<TableDataCollection>();
+
+
+        public static void ReadTable(IWebElement table)
+        {
+            //Get all the columns from the table
+            var columns = table.FindElements(By.TagName("th"));
+
+            //Get all the rows
+            var rows = table.FindElements(By.TagName("tr"));
+
+            //Create the row index
+            int rowIndex = 0;
+
+            foreach (var row in rows)
+            {
+                int colIndex = 0;
+
+                var colDatas = row.FindElements(By.TagName("td"));
+
+                foreach (var colValue in colDatas)
+                {
+                    _tableDataCollection.Add(new TableDataCollection
+                    {
+                        RowNumber = rowIndex,
+                        //ColumnName = columns[colIndex].Text,
+                    //For complex tables
+                        ColumnName = columns[colIndex].Text != "" ?
+                                     columns[colIndex].Text : colIndex.ToString(),
+                    //..
+                        ColumnValue = colValue.Text,
+                        //For complex tables
+                        ColumnSpecialValues = colValue.Text != "" ? null :
+                                              colValue.FindElements(By.TagName("input"))
+                    //..
+                    });
+
+                    //Move to next column
+                    colIndex++;
+                }
+                rowIndex++;
+            }
+        }
+
+        public static string ReadCell(string columnName, int rowNumber)
+        {
+            var data = (from e in _tableDataCollection
+                        where e.ColumnName == columnName && e.RowNumber == rowNumber
+                        select e.ColumnValue).SingleOrDefault();
+            return data;
+        }
+
+    //For complex tables
+        public static void PerformActionOnCell(string columnIndex, string refColumnName, string refColumnValue, string controlToOperate = null)
+        {
+            foreach (int rowNumber in GetDynamicRowNumber(refColumnName,refColumnValue))
+            {
+                var cell = (from e in _tableDataCollection
+                            where e.ColumnName == columnIndex && e.RowNumber == rowNumber
+                            select e.ColumnSpecialValues).SingleOrDefault();
+            
+
+                //Operate on those controls
+                if(controlToOperate != null && cell != null)
+                {
+                    var returnedControl = (from c in cell
+                                           where c.GetAttribute("value") == controlToOperate
+                                           select c).SingleOrDefault();
+
+                    returnedControl?.Click();
+                }
+                else
+                {
+                    cell?.First().Click();
+                }
+            }
+        }
+
+        public static IEnumerable GetDynamicRowNumber(string columnName, string columnValue)
+        {
+            //dynamic row
+            foreach (var table in _tableDataCollection)
+            {
+                if (table.ColumnName == columnName && table.ColumnValue == columnValue)
+                {
+                    yield return table.RowNumber;
+                }
+            }
+        }
+    //..
+    }
+
+    public class TableDataCollection
+    {
+        public int RowNumber { get; set; }
+
+        public string ColumnName { get; set; }
+
+        public string ColumnValue { get; set; }
+
+    //For Complex tables
+        public IEnumerable<IWebElement> ColumnSpecialValues { get; set; }
+    //..
+    }
+}
