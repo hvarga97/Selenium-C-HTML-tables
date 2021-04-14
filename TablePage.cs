8eff5e91264208637612731caf8472596a5372f1
Hrvoje <hvarga97@gmail.com>
2021-04-14 18:19:42 +0200

    Add files via upload

---
+++ TablePage.cs
@@ -0,0 +1,23 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReadingTable
+{
+    public class TablePage : ReadingTable.Base
+    {
+        public TablePage()
+        {
+            PageFactory.InitElements(Driver, this);
+        }
+
+        [FindsBy(How = How.XPath, Using ="//table")]
+
+        public IWebElement table { get; set; }
+
+    }
+}
